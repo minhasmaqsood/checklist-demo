1c432f95aa673cb389eed6778430db94f58adcc3
Minhas Maqsood <minhasmaqsood@gmail.com>
2022-03-15 17:15:22 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {Fragment} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+function App() {
+  return (
+   <Fragment>
+       <header className="App App-header">
+           CHECKLIST
+       </header>
+       <div className="container">
+           <div className="row">
+
+           </div>
+       </div>
+   </Fragment>
+  );
+}
+
+export default App;
